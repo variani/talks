160c4c0b97a1e6a67b17ebde0137ca3d899c4ccb
variani <machovariani@gmail.com>
2016-05-17 13:20:50 +0200

    final

--- 2016/01-lme4qtl-eshg16/R/17-throm-risk-model-pfa.R
+++ 2016/01-lme4qtl-eshg16/R/17-throm-risk-model-pfa.R
@@ -13,10 +13,10 @@ K <- 0.05
 dat <- mutate(phen2, offset = log(K/(1 - K)))
 
 dat <- within(dat, {
-  PFAadp_q10 <- factor(ifelse(PFAadp < quantile(PFAadp, 0.1, na.rm =  TRUE), "low", "high"))
-  PFAepin_q10 <- factor(ifelse(PFAepin < quantile(PFAepin, 0.1, na.rm =  TRUE), "low", "high"))
-  PFAadp_q20 <- factor(ifelse(PFAadp < quantile(PFAadp, 0.2, na.rm =  TRUE), "low", "high"))
-  PFAepin_q20 <- factor(ifelse(PFAepin < quantile(PFAepin, 0.2, na.rm =  TRUE), "low", "high"))
+  PFAadp_q10 <- factor(ifelse(PFAadp < quantile(PFAadp[Throm == 1], 0.1, na.rm =  TRUE), "low", "high"))
+  PFAepin_q10 <- factor(ifelse(PFAepin < quantile(PFAepin[Throm == 1], 0.1, na.rm =  TRUE), "low", "high"))
+  PFAadp_q20 <- factor(ifelse(PFAadp < quantile(PFAadp[Throm == 1], 0.2, na.rm =  TRUE), "low", "high"))
+  PFAepin_q20 <- factor(ifelse(PFAepin < quantile(PFAepin[Throm == 1], 0.2, na.rm =  TRUE), "low", "high"))
 })
 
 dat <- mutate(dat, 
@@ -40,6 +40,22 @@ m20 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + (1|ID), dat2,
   offset = offset, relmat = list(ID = dkin2), family = binomial)
 m2 <- update(m20, . ~ . + PFAepin_q10num)
 
+### AB0 (rs8176719)
+dat1 <- subset(dat, !is.na(PFAadp_q10num) & !is.na(rs8176719))
+
+mod10 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + rs8176719 + (1|ID), dat1, 
+  offset = offset, relmat = list(ID = dkin2), family = binomial)
+mod1 <- update(mod10, . ~ . + PFAadp_q10num)
+
+dat2 <- subset(dat, !is.na(PFAepin_q10num) & !is.na(rs8176719))
+
+mod20 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + rs8176719 + (1|ID), dat2, 
+  offset = offset, relmat = list(ID = dkin2), family = binomial)
+mod2 <- update(mod20, . ~ . + PFAepin_q10num)
+
+if(FALSE) 
+{
+
 ### AB0 
 dat1 <- subset(dat, !is.na(PFAadp_q10num) & !is.na(ABOf3num))
 
@@ -53,19 +69,6 @@ mod20 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + ABOf3num + (1|ID), 
   offset = offset, relmat = list(ID = dkin2), family = binomial)
 mod2 <- update(mod20, . ~ . + PFAepin_q10num)
 
-
-### AB0 (rs8176719)
-dat1 <- subset(dat, !is.na(PFAadp_q10num) & !is.na(rs8176719))
-
-mod10 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + rs8176719 + (1|ID), dat1, 
-  offset = offset, relmat = list(ID = dkin2), family = binomial)
-mod1 <- update(mod10, . ~ . + PFAadp_q10num)
-
-dat2 <- subset(dat, !is.na(PFAepin_q10num) & !is.na(rs8176719))
-
-mod20 <- relmatGlmer(Throm ~ -1 + AGEsc + AGEsc2 + SEXfnum + rs8176719 + (1|ID), dat2, 
-  offset = offset, relmat = list(ID = dkin2), family = binomial)
-mod2 <- update(mod20, . ~ . + PFAepin_q10num)
 
 ### AB0 (rs8176719) / AGEsc2 skipped
 dat1 <- subset(dat, !is.na(PFAadp_q10num) & !is.na(rs8176719))
@@ -82,3 +85,4 @@ mod2 <- update(mod20, . ~ . + PFAepin_q10num)
 
 # exp(cbind(fixef(mod2), confint(mod2, method = "Wald", parm = names(fixef(mod2)))))
 
+}
